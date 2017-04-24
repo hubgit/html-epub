0dffb1a0d96bfbd5ca39439bc3e84f74c0ba0b7e
Alf Eaton <eaton.alf@gmail.com>
2017-04-24 16:45:23 +0100

    Initial commit

---
+++ test/test.js
@@ -0,0 +1,95 @@
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 100000
+const fs = require('fs')
+const path = require('path')
+const glob = require('glob')
+const os = require('os')
+const uuid = require('uuid')
+// const exec = require('child-process-promise').exec
+
+const HTMLEPUB = require('../lib')
+
+const htmlFiles = glob.sync(path.join(__dirname, '..', 'data', '*.html')).map(file => ({
+  basename: path.basename(file),
+  content: fs.readFileSync(file)
+}))
+
+const htmlFile = htmlFiles.find(item => item.basename === '1.html')
+
+const book = {
+  title: 'Test Book',
+  identifier: 'com.example/1',
+  updated: '2017-01-01T00:00:00Z'
+}
+
+test('generates metadata', () => {
+  const epub = new HTMLEPUB(book)
+
+  expect(epub.metadata['dc:title']['#text']).toBe(book.title)
+})
+
+test('parses html', (done) => {
+  const epub = new HTMLEPUB(book)
+
+  epub.parseHTML(htmlFile.content).then($ => {
+    try {
+      expect($('h2').text()).toBe('Test Section')
+      done()
+    } catch (e) {
+      done.fail(e)
+    }
+  })
+})
+
+test('produces xhtml', (done) => {
+  const epub = new HTMLEPUB(book)
+
+  epub.parseHTML(htmlFile.content).then($ => {
+    try {
+      expect($('html').attr('xmlns')).toBe('http://www.w3.org/1999/xhtml')
+      done()
+    } catch (e) {
+      done.fail(e)
+    }
+  })
+})
+
+test('extracts files', (done) => {
+  const epub = new HTMLEPUB(book)
+
+  epub.loadHTMLFiles(htmlFiles).then(() => {
+    try {
+      const image = epub.images.find(item => item.source === '/images/1.png')
+
+      expect(image).not.toBeNull()
+      done()
+    } catch (e) {
+      done.fail(e)
+    }
+  })
+})
+
+test('creates a zip file', (done) => {
+  const epub = new HTMLEPUB(book)
+
+  epub.loadHTMLFiles(htmlFiles).then(() => {
+    const outputFile = os.tmpdir() + '/' + uuid() + '.epub'
+    // const outputFile = os.tmpdir() + '/' + 'test.epub'
+    console.log('Writing to', outputFile)
+
+    const outputStream = fs.createWriteStream(outputFile)
+
+    epub.stream(outputStream).then((archive) => {
+      outputStream.on('close', function () {
+        console.log('Wrote', outputStream.bytesWritten, 'bytes')
+
+        // exec('epubcheck ' + outputFile).then(result => {
+        //   console.log(result.stdout)
+        //   console.err(result.stderr)
+        //   done()
+        // }).catch(done.fail)
+
+        done()
+      })
+    }).catch(done.fail)
+  }).catch(done.fail)
+})
